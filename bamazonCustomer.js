59a4193f121460ce1108efa01e34e1cb32ad1a4e
Julian Hasse <info@julianhasse.com>
2017-11-11 15:59:38 -0500

    added UPDATE function

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -7,21 +7,21 @@
 
 
 // ================ Packages =======================
-var inquirer = require('inquirer');
+const inquirer = require('inquirer');
 // A collection of common interactive command line user interfaces.
-var mysql = require('mysql');
+const mysql = require('mysql');
 // This is a node.js driver for mysql.
-var Table = require('cli-table');
+const Table = require('cli-table');
 // Renders unicode-aided tables on the command line from your node.js scripts.
-var chalk = require('chalk');
+const chalk = require('chalk');
 // Terminal string styling done right.
-var figlet = require("figlet"); 
+const figlet = require("figlet"); 
 // Implements the FIGfont spec in JavaScript.
 
 
 
 // =============== Database Credentials ============
-var connection = mysql.createConnection({
+const connection = mysql.createConnection({
     user: 'root',
     password: '91zv4999',
     host: 'localhost',
@@ -45,8 +45,8 @@ function displayItems(){
         if (err){
             throw err;
         } else {
-            console.log("connected as id " + connection.threadId + "\n");
-            // console.log(results);
+            console.log("        New and Interesting finds in Bamazon: \n");
+           
             var table = new Table({
                 head:['ID', 'Product', 'Department', 'Price', 'Quantity'],
             });
@@ -55,10 +55,66 @@ function displayItems(){
                 table.push([results[i].item_id, results[i].product_name, results[i].department_name, results[i].price, results[i].stock_quantity]);
             }
             console.log(chalk.bold.yellow(table.toString()));
-            quit();
+            promptUser(results);
         }
     })
 };
+
+function promptUser(results){
+    inquirer.prompt([{
+            type: "input",
+            message: "Please enter the product ID of the item you would like to purchase\n",
+            name: "productId",
+            validate: function(value) {
+                if (isNaN(value) === false){
+                    return true;
+                }
+                    return false;
+            }
+        },
+        {
+            type: "input",
+            message: "Please enter how many units you would like to purchase\n",
+            name: "productUnits",
+            validate: function(value) {
+                if (isNaN(value) === false){
+                    return true;
+                }
+                    return false;
+            }  
+        }
+    ]).then(function(response){
+        var selectedItem = "";
+        var selectedUnits = parseInt(response.productUnits);
+
+        for (var i = 0; i < results.length; i++){
+            if (results[i].item_id == response.productId) {
+                selectedItem = results[i];
+                break;
+            }
+        }
+        if (selectedItem.stock_quantity >= selectedUnits) {
+            var updateQuantity = selectedItem.stock_quantity - selectedUnits;
+            connection.query('UPDATE products SET ? WHERE ?', [{
+                stock_quantity: updateQuantity
+            }, {
+                item_id: selectedItem.item_id
+
+            }],
+            function(err){
+                if (err) throw err;
+                var totalAmount = selectedUnits * selectedItem.price;
+                console.log("Your total is: $" + totalAmount);
+                quit();
+            }
+        );
+        } else {
+            console.log("We are sorry. There are not enough of: \n" + chalk.red(selectedItem.product_name) + "\n" + "in stock to complete your order")
+            quit();
+        }
+    });  
+}
+
 
 function quit(){
 connection.end();
